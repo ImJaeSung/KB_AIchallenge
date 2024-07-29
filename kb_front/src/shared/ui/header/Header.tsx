15d5fca9c3091f67aaa8bd87551728b6a37610a2
송준섭 <wnstjq7979@gmail.com>
2024-07-29 21:37:18 +0900

    feat: Header 구현

--- kb_front/src/shared/ui/header/Header.tsx
+++ kb_front/src/shared/ui/header/Header.tsx
@@ -1,7 +1,44 @@
+import styled from "styled-components";
+
+const HeaderContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  width: 100%;
+  height: 530px;
+
+  background-color: #effce6;
+`;
+
+const HeaderInnerDiv = styled.div`
+  display: block;
+  justify-content: center;
+
+  padding: 3px;
+  width: 980px;
+  height: 100%;
+`;
+
+const HeaderMenuContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  width: 100%;
+  height: 32px;
+  margin: 0 12px;
+
+  background-color: #cf4242;
+`;
+
 export default function Header() {
   return (
-    <div>
-      <h1>Header</h1>
-    </div>
+    <HeaderContainer>
+      <HeaderInnerDiv>
+        <HeaderMenuContainer />
+        <h1>Header</h1>
+      </HeaderInnerDiv>
+    </HeaderContainer>
   );
 }
