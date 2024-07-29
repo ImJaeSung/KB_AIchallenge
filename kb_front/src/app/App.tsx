c9c91200631c9d5404cbcf4854f58d2f41cb9d68
송준섭 <wnstjq7979@gmail.com>
2024-07-29 21:26:28 +0900

    feat: Header.tsx 추가

--- kb_front/src/app/App.tsx
+++ kb_front/src/app/App.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Header } from "shared/ui";
 import { HomePage } from "pages";
 import "./global.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
+    <Header />
     <h1>Hello, World!</h1>
     <HomePage />
   </React.StrictMode>,
