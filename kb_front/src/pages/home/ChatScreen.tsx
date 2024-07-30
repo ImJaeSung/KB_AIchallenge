875a288d77811ed3f7ddc88d3e69558e0c98f9a0
송준섭 <wnstjq7979@gmail.com>
2024-07-30 16:21:59 +0900

    feat: ChatContentInput 부분 구현

--- kb_front/src/pages/home/ChatScreen.tsx
+++ kb_front/src/pages/home/ChatScreen.tsx
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import chatbot from "assets/home/chatbot.png";
+import send from "assets/home/send.png";
 
 const ChatScreenContainer = styled.div`
   position: fixed;
@@ -11,7 +12,6 @@ const ChatScreenContainer = styled.div`
   color: #fff;
   background-color: rgba(255, 255, 255, 0.8);
   box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-  border-radius: 10px;
 `;
 
 const ChatScreenHeader = styled.div`
@@ -21,6 +21,9 @@ const ChatScreenHeader = styled.div`
   padding: 5px 10px;
   border-bottom: 1px solid #e5e5e5;
   color: #000;
+
+  background-color: rgba(238, 238, 174, 0.8);
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
 `;
 
 const ChatScreenHeaderButtons = styled.div`
@@ -39,6 +42,59 @@ const ChatScreenCloseButton = styled.button`
   color: #000;
   font-size: 20px;
   cursor: pointer;
+`;
+
+const ChatScreenContentContainer = styled.div`
+  height: 427px;
+  overflow-y: auto;
+  color: #000;
+`;
+
+const ChatScreenContentInputContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  width: 100%;
+  height: 40px;
+  border-top: 1px solid #e5e5e5;
+  border-left: none;
+  border-right: none;
+  border-bottom: none;
+  outline: none;
+  font-size: 16px;
+
+  background-color: rgba(238, 238, 174, 0.8);
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+`;
+
+const ChatScreenContentInput = styled.input`
+  width: 360px;
+  height: 100%;
+  padding: 0 10px;
+  border: none;
+  background-color: transparent;
+  outline: none;
+
+  &::placeholder {
+    color: #a78686;
+    opacity: 0.5;
+  }
+`;
+
+const ChatScreenInputSendButton = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  width: 40px;
+  height: 100%;
+  background-color: #fff;
+  border: none;
+
+  &:hover {
+    background-color: #e0dbdb;
+  }
 `;
 
 export default function ChatScreen({ isChatScreenOpen, setIsChatScreenOpen }) {
@@ -64,6 +120,13 @@ export default function ChatScreen({ isChatScreenOpen, setIsChatScreenOpen }) {
               </ChatScreenCloseButton>
             </ChatScreenHeaderButtons>
           </ChatScreenHeader>
+          <ChatScreenContentContainer>zz</ChatScreenContentContainer>
+          <ChatScreenContentInputContainer>
+            <ChatScreenContentInput placeholder="금융의 뜻이 뭐야?" />
+            <ChatScreenInputSendButton>
+              <img src={send} />
+            </ChatScreenInputSendButton>
+          </ChatScreenContentInputContainer>
         </ChatScreenContainer>
       ) : null}
     </>
