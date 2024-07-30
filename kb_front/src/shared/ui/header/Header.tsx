1acc87547916adf38c8c76defe135701241fb4b8
송준섭 <wnstjq7979@gmail.com>
2024-07-30 09:56:28 +0900

    feat: Header에 menus 구현

--- kb_front/src/shared/ui/header/Header.tsx
+++ kb_front/src/shared/ui/header/Header.tsx
@@ -1,4 +1,8 @@
 import styled from "styled-components";
+import logo from "assets/logo.png";
+import glasses from "assets/glasses.png";
+import border from "assets/border.png";
+import vertical from "assets/vertical.png";
 
 const HeaderContainer = styled.div`
   display: flex;
@@ -28,16 +32,35 @@ const HeaderMenuContainer = styled.div`
   width: 100%;
   height: 32px;
   margin: 0 12px;
+`;
 
-  background-color: #cf4242;
+const HeaderMenus = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 640px;
+  height: 32px;
+
+  font-size: 14px;
+  font-weight: 400;
 `;
 
 export default function Header() {
   return (
     <HeaderContainer>
       <HeaderInnerDiv>
-        <HeaderMenuContainer />
-        <h1>Header</h1>
+        <HeaderMenuContainer>
+          <img src={logo} />
+          <HeaderMenus>
+            개인 기업 <img src={vertical} style={{ margin: "0 8px" }} />
+            자산관리 부동산 퇴직연금 카드{" "}
+            <img src={vertical} style={{ margin: "0 5px" }} /> 전체 서비스{" "}
+            <img src={border} style={{ margin: "0 5px" }} />{" "}
+            <img src={vertical} style={{ margin: "0 5px" }} /> Global{" "}
+            <img src={border} style={{ margin: "0 5px" }} />
+            <img src={glasses} />
+          </HeaderMenus>
+        </HeaderMenuContainer>
       </HeaderInnerDiv>
     </HeaderContainer>
   );
