582ba5ecbb478a01c1dc5ddd9c07414720c18fa7
송준섭 <wnstjq7979@gmail.com>
2024-07-30 09:35:47 +0900

    feat: members router 추가

--- kb_backend/main.py
+++ kb_backend/main.py
@@ -1,8 +1,15 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from routers import members
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
-@app.get("/")
-async def read_root():
-    return {"Hello": "World"}
+app.include_router(members.router, prefix="/members", tags=["members"])
