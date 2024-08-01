f1eef426d17075a9b6877a129598ee5ecc5ec2fa
choitaesoon@gmail.com <cts0817@naver.com>
2024-08-01 14:42:28 +0900

    test_contents 1,2

--- modules/Cosine_Similarity.py
+++ modules/Cosine_Similarity.py
@@ -7,6 +7,10 @@ class CosineSimilarityCalculator:
 
 
     def cosine_similarity(self, vec1, vec2):
+
+        vec1 = np.squeeze(vec1)
+        vec2 = np.squeeze(vec2)
+
         dot_product = np.dot(vec1, vec2)
         norm_vec1 = np.linalg.norm(vec1)
         norm_vec2 = np.linalg.norm(vec2)
